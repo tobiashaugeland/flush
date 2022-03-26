602821aa223f3fb84bcca7e4e84c72ef6493ba50
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2022-03-26 12:56:19 +0100

    It can now make folders

--- flush.c
+++ flush.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+
+int make_dir(const char *pathname, mode_t mode)
+{
+    int ret;
+    ret = mkdir(pathname, mode);
+    if (ret == -1)
+    {
+        perror("could not make directory");
+        return -1;
+    }
+    return 0;
+}
 
 int main()
 {
-    int i;
-    for(i=0;i<10;i++)
-    {
-        printf("%d\n",i);
-        fflush(stdout);
-        sleep(1);
-    }
-    return 0;
+    char buf[1024];
+    fgets(buf, sizeof(buf), stdin);
+    make_dir(buf,0777);
 }
